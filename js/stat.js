07e64c041ca4df497e871a6e5af9687aa3920b5f
Artem <temaspsxxx@gmail.com>
2019-09-15 15:30:58 +0300

    Выполнил отрисовку экрана статистики согласно ТЗ

--- js/stat.js
+++ js/stat.js
@@ -4,17 +4,20 @@ var CLOUD_WIDTH = 420;
 var CLOUD_HEIGHT = 270;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
+var GRAPH_Y = 80;
 var GAP = 10;
 var FONT_GAP = 15;
-var TEXT_WIDTH = 50;
-var BAR_HEIGHT = 20;
-var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var BAR_HEIGHT = 150;
+var BAR_WIDTH = 40;
+var BAR_GAP = 50;
 
+// Рисование облака
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
+// Поиск максимального элемента в массиве
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
@@ -49,14 +52,28 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.textBaseline = 'alphabetic';
 
   for (var i = 0; i < names.length; i++) {
-    //   MAX_BAR      BAR[i]
-    // ----------- = --------
-    //  BAR_WIDTH        X
+    //   MAX_BAR       BAR[i]
+    // ------------ = --------
+    //  BAR_HEIGHT        X
 
-    // X = (BAR_WIDTH * BAR[i]) / MAX_BAR
+    // X = (BAR_HEIGHT * BAR[i]) / MAX_BAR
 
-    ctx.fillText(names[i], CLOUD_X + GAP, CLOUD_Y + 100 + GAP + FONT_GAP + (GAP + BAR_HEIGHT) * i);
-    ctx.fillRect(CLOUD_X + GAP + TEXT_WIDTH, CLOUD_Y + 100 + GAP + (GAP + BAR_HEIGHT) * i, (barWidth * times[i]) / maxTime, BAR_HEIGHT);
+    var bar = (BAR_HEIGHT * times[i] / maxTime); // текущее значение столбца гистограмы
+    ctx.fillStyle = '#000';
+    ctx.fillText(names[i], CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + CLOUD_HEIGHT - GAP);
+    ctx.fillText(Math.round(times[i]), CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + GRAPH_Y + (BAR_HEIGHT - bar));
+
+    // Столбец игрока красным цветом, остальные синим с разной насыщенностью
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      var saturation = 100 * Math.random();
+      ctx.fillStyle = 'hsla(240,' + saturation + '%, 50%, 1)';
+    }
+
+    ctx.translate(0, (BAR_HEIGHT - bar));
+    ctx.fillRect(CLOUD_X + BAR_GAP + (BAR_GAP + BAR_WIDTH) * i, CLOUD_Y + GRAPH_Y + GAP, BAR_WIDTH, bar);
+    ctx.setTransform(1, 0, 0, 1, 0, 0); // Reset current transformation matrix to the identity matrix
   }
 
 };
