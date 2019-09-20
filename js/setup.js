0c16243431d15c823462d482c97623fbfa807aa0
Artem <temaspsxxx@gmail.com>
2019-09-20 13:57:45 +0300

    Открытие/закрытие окна настройка персонажа

--- js/setup.js
+++ js/setup.js
@@ -4,9 +4,11 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+
 
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
@@ -55,3 +57,66 @@ for (i = 0; i < wizards.length; i++) {
 similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+
+// 1.  Открытие/закрытие окна настройки персонажа:
+// -  Окно.setup должно открываться по нажатию на блок.setup-open.
+// Открытие окна производится удалением класса hidden у блока
+var setupOpen = document.querySelector('.setup-open');
+var setup = document.querySelector('.setup');
+var setupClose = setup.querySelector('.setup-close');
+var userNameInput = setup.querySelector('.setup-user-name');
+
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE && evt.target !== userNameInput) {
+    closePopup();
+  }
+}
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+// Добавить обработчики для альтернативного ввода с
+// клавиатуры keydown для кнопок открытия/закрытия диалога настройки персонажа:
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+// 3.  Когда иконка пользователя в фокусе .setup-open-icon, то окно
+// настройки персонажа должно открываться по нажатию кнопки ENTER
+// Не забудьте добавить tabindex="0" для иконки пользователя, чтобы
+// она фокусировалась.
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+// -  Окно.setup должно закрываться по нажатию на элемент.setup-close,
+// расположенный внутри окна. Нажатие на элемент .setup-close, расположенный
+// внутри блока setup возвращает ему класс hidden.
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+// 4.  Когда окно настройки персонажа открыто, нажатие на
+// клавишу ESC должно закрывать диалог
+// Если фокус находится на форме ввода имени, то окно закрываться
+// не должно.
+// 5.  Если окно открыто и фокус находится на кнопке закрытия окна, то
+// нажатие клавиши ENTERдолжно приводить к закрытию диалога
+// 6.  Если диалог открыт, нажатие на кнопку «Сохранить» приводит к
+// отправке формы
+// 7.  Если диалог открыт и фокус находится на кнопке «Сохранить»,
+// нажатие на ENTER приводит к отправке формы
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+
