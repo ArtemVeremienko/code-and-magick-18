0c8ddc863e32d24a5d0b4dbcf02d306cbd3720b9
Artem <temaspsxxx@gmail.com>
2019-09-20 13:58:37 +0300

    Валидация ввода имени персонажа

--- js/setup.js
+++ js/setup.js
@@ -119,4 +119,29 @@ setupClose.addEventListener('keydown', function (evt) {
   }
 });
 
+// 2.  Валидация ввода имени персонажа. Имя персонажа вводится в
+// поле .setup-user-name. Добавьте следующие ограничения:
+// -  имя персонажа не может содержать менее 2 символов;
+// -  максимальная длина имени персонажа — 25 символов.
+// Перевод ошибки на русский язык
 
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
