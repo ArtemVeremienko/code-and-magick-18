45df0e230c6383230be3d1d11f81a0b654bc8a50
Artem <temaspsxxx@gmail.com>
2019-10-29 01:39:04 +0200

    Модуль для устранения дребезга магов

---
+++ js/debounce.js
@@ -0,0 +1,21 @@
+'use strict';
+
+(function () {
+  var DEBOUNCE_INTERVAL = 300;
+
+  window.debounce = function (fun) {
+    var lastTimeout = null;
+
+    return function () {
+      var args = arguments;
+
+      if (lastTimeout) {
+        window.clearTimeout(lastTimeout);
+      }
+
+      lastTimeout = window.setTimeout(function () {
+        fun.apply(null, args);
+      }, DEBOUNCE_INTERVAL);
+    };
+  };
+})();
