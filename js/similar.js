74bc71741329a60dd06d761babe62e44a558da7a
Artem <temaspsxxx@gmail.com>
2019-10-29 01:37:19 +0200

    Модуль для поиска похожих магов

--- js/similar.js
+++ js/similar.js
@@ -1,58 +1,60 @@
 'use strict';
 
 (function () {
-  var FIREBALL_BACKGROUND = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
-
-  var setup = document.querySelector('.setup');
-  var setupWizard = setup.querySelector('.setup-wizard-appearance');
-  var setupWizardCoat = setupWizard.querySelector('.wizard-coat');
-  var setupInputWizardCoat = setupWizard.querySelector('input[name=coat-color]');
-  var setupWizardEyes = setupWizard.querySelector('.wizard-eyes');
-  var setupInputWizardEyes = setupWizard.querySelector('input[name=eyes-color]');
-  var setupFireball = setup.querySelector('.setup-fireball-wrap');
-  var setupInputFireball = setupFireball.querySelector('input[name=fireball-color]');
-
-  // 3. Изменение цвета мантии персонажа по нажатию. Цвет мантии setup-wizard-appearance
-  // .wizard-coat должен обновляться по нажатию на неё. Цвет мантии задаётся через
-  // изменение инлайнового CSS-свойства fill для элемента.
-
-  // Случайно изменяем цвет (с массива) переданного элемента itemName + передаём его значение в inputName
-  var setWizardColor = function (arrayOfVariables, itemName, inputName) {
-    var randomItem = window.util.getRandomElement(arrayOfVariables);
-    inputName.value = randomItem;
-    if (itemName.tagName.toLowerCase() === 'div') { // в фаерболе меняем цвет background вместо fill
-      itemName.style.backgroundColor = randomItem;
-    } else {
-      itemName.style.fill = randomItem;
-    }
-  };
+  var wizards = [];
 
   // Запоминаю текущий выбранный цвет куртки или глаз, при его смене
   var coatColor;
   var eyesColor;
 
-  setupWizardCoat.addEventListener('click', function () {
-    setWizardColor(coatColor = window.util.WIZARD_COAT_COLOR, setupWizardCoat, setupInputWizardCoat);
-  });
+  // Система отличности одного мага от другого
+  var getRank = function (wizard) {
+    var rank = 0;
 
-  // 4.  Изменение цвета глаз персонажа по нажатию. Цвет глаз волшебника
-  // меняется по нажатию на блок .setup-wizard .wizard-eyes.
-  setupWizardEyes.addEventListener('click', function () {
-    setWizardColor(window.util.WIZARD_EYES_COLOR, setupWizardEyes, setupInputWizardEyes);
-  });
+    if (wizard.colorCoat === coatColor) {
+      rank += 2;
+    }
 
-  // 5.  Изменение цвета фаерболов по нажатию. Цвет задаётся через
-  // изменение фона у блока .setup-fireball-wrap.
-  setupFireball.addEventListener('click', function () {
-    setWizardColor(FIREBALL_BACKGROUND, setupFireball, setupInputFireball);
-  });
+    if (wizard.colorEyes === eyesColor) {
+      rank += 1;
+    }
 
+    return rank;
+  }
 
-  var wizards = [];
+  // Сортировка магов по имени (в случае когда ранк магов одинаков)
+  var namesComparator = function (leftName, rightName) {
+    if (leftName > rightName) {
+      return 1;
+    } else if (leftName < rightName) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
+  var wizardsComparator = function (left, right) {
+    var rankDiff = getRank(right) - getRank(left);
+    return rankDiff === 0 ? namesComparator(left.name, right.name) : rankDiff;
+  };
+
+  var updateFilter = function () {
+    window.render(wizards.sort(wizardsComparator));
+  };
+
+  window.wizard.onCoatChange = function (color) {
+    coatColor = color;
+    updateFilter();
+  };
+
+  window.wizard.onEyesChange = function (color) {
+    eyesColor = color;
+    updateFilter();
+  };
 
   var successHandler = function (data) {
     wizards = data;
-    window.render(wizards);
+    updateFilter();
   };
 
   var errorHandler = function (errorMessage) {
